51bfe2cdb040f28d237554cf4a0c2edfbd5cdc51
unknown <faizanbabar70@yahoo.com>
2023-06-24 05:31:21 +0500

    Add

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -22,6 +22,7 @@ import {
 import { LIMIT_ORDER_HELPER_ADDRESS, STOP_LIMIT_ORDER_ADDRESS } from '@sushiswap/limit-order-sdk'
 import MISO from '@sushiswap/miso/exports/all.json'
 import ConstantProductPoolArtifact from '@sushiswap/trident/artifacts/contracts/pool/constant-product/ConstantProductPool.sol/ConstantProductPool.json'
+//@ts-ignore
 import TRIDENT from '@sushiswap/trident/exports/all.json'
 import { Pool, PoolType } from '@sushiswap/trident-sdk'
 import {
